fa4579f5bf330f1d2bc357ceacff6ed37da6c36e
Rahul JAin <rahul.jain@appventurez.com>
2024-08-26 11:57:24 +0530

    add math feature

--- src/MyEditor.js
+++ src/MyEditor.js
@@ -1,17 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Import Quill styles
 
 const MyEditor = () => {
   const [editorContent, setEditorContent] = useState('');
 
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML';
+    script.async = true;
+    document.body.appendChild(script);
+    
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   const handleChange = (value) => {
     setEditorContent(value);
   };
 
+  useEffect(() => {
+    if (window.MathJax) {
+      window.MathJax.Hub.Queue(['Typeset', window.MathJax.Hub]);
+    }
+  }, [editorContent]);
+
+
+  const handleInsertMath = () => {
+    const mathInput = prompt('Enter LaTeX math equation:');
+    if (mathInput) {
+      setEditorContent(editorContent + `\\(${mathInput}\\)`);
+    }
+  };
+
+
   return (
     <div>
       <h2>React Quill Editor</h2>
+      <button onClick={handleInsertMath}>Insert Math</button>
       <ReactQuill
         value={editorContent}
         onChange={handleChange}
